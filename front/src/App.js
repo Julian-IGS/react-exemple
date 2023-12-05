2a5af8de9338638d06f939d20d609562c8daf09b
Jeremy Brenet <jrm.devweb@gmail.com>
2023-12-05 11:17:17 +0100

    feat(app): add request pokemon

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+
+
+
 
 function App() {
+  const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    const backendServiceUrl = "http://localhost:5000"
+    fetch(`${backendServiceUrl}/api/pokemon`)
+        .then(response => response.json())
+        .then(data => setPokemon(data) )
+        .catch(error => console.error(error))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            TP kubernetes
+          </a>
+          { pokemon &&
+
+
+              <div>
+                <h1>
+                  Pokemon
+                </h1>
+                <h2>
+                  Name : {pokemon.name}
+                </h2>
+                <h2>
+                  Type : {pokemon.type}
+                </h2>
+              </div>
+          }
+        </header>
+      </div>
   );
 }
 
